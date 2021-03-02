5ed0a30341d24c9fda492537e3cb10dc1421a8ae
Muller Esposito <mulleresposito@hotmail.com>
2021-03-02 03:39:44 -0300

    feat(MovieLibrary): creates movielibrary component;

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,72 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleOnClickAddMovie = this.handleOnClickAddMovie.bind(this);
+  }
+
+  handleOnChange({ target }) {
+    const { movies } = this.props;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+
+    this.setState({
+      [name]: value,
+      movies,
+    });
+  }
+
+  handleOnClickAddMovie(movie) {
+    const { movies } = this.props;
+
+    this.setState({ movies: [...movies, movie] });
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return (
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          checked={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          onSearchTextChange={ this.handleOnChange }
+          onBookMarkedChange={ this.handleOnChange }
+          onSelectedGenreChange={ this.handleOnChange }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie onClick={ this.handleOnClickAddMovie } />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      subtitle: PropTypes.string,
+      title: PropTypes.string,
+      imagePath: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      genre: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+export default MovieLibrary;
