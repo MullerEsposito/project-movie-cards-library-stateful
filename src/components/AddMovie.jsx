66fea9ef7b473c289117b735b768fdd374e31965
Muller Esposito <mulleresposito@hotmail.com>
2021-03-02 00:01:55 -0300

    refactor(AddMovie): modularizes the form fields in functions;

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -36,53 +36,123 @@ class AddMovie extends Component {
     });
   }
 
+  renderInputTitle() {
+    const { title } = this.state;
+    return (
+      <label data-testid="title-input-label" htmlFor="title-input">
+        Título
+        <input
+          name="title"
+          data-testid="title-input"
+          value={ title }
+          onChange={ this.handleInputOnChange }
+        />
+      </label>
+    );
+  }
+
+  renderInputSubTitle() {
+    const { subtitle } = this.state;
+    return (
+      <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
+        Subtítulo
+        <input
+          name="subtitle"
+          data-testid="subtitle-input"
+          value={ subtitle }
+          onChange={ this.handleInputOnChange }
+        />
+      </label>
+    );
+  }
+
+  renderInputImage() {
+    const { imagePath } = this.state;
+    return (
+      <label data-testid="image-input-label" htmlFor="image-input">
+        Imagem
+        <input
+          name="imagePath"
+          data-testid="image-input"
+          value={ imagePath }
+          onChange={ this.handleInputOnChange }
+        />
+      </label>
+    );
+  }
+
+  renderInputStoryline() {
+    const { storyline } = this.state;
+    return (
+      <label data-testid="storyline-input-label" htmlFor="storyline-input">
+        Sinopse
+        <textarea
+          name="storyline"
+          data-testid="storyline-input"
+          value={ storyline }
+          onChange={ this.handleInputOnChange }
+        />
+      </label>
+    );
+  }
+
+  renderInputRating() {
+    const { rating } = this.state;
+    return (
+      <label data-testid="rating-input-label" htmlFor="rating-input">
+        Avaliação
+        <input
+          type="number"
+          name="rating"
+          data-testid="rating-input"
+          onChange={ this.handleInputOnChange }
+          value={ rating }
+        />
+      </label>
+    );
+  }
+
+  renderInputGenre() {
+    const { genre } = this.state;
+    return (
+      <label data-testid="genre-input-label" htmlFor="genre-input">
+        Gênero
+        <select
+          name="genre"
+          data-testid="genre-input"
+          value={ genre }
+          onChange={ this.handleInputOnChange }
+        >
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  renderSendButton() {
+    return (
+      <button
+        type="button"
+        data-testid="send-button"
+        onClick={ this.handleButtonOnClick }
+      >
+        Adicionar filme
+      </button>
+    );
+  }
+
   render() {
-    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
-    const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" htmlFor="title-input">
-          Título
-          <input name="title" type="text" data-testid="title-input" value={ title } onChange={ this.handleInputOnChange } />
-        </label>
-        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
-          Subtítulo
-          <input name="subtitle" data-testid="subtitle-input" value={ subtitle } onChange={ this.handleInputOnChange } />
-        </label>
-        <label data-testid="image-input-label" htmlFor="image-input">
-          Imagem
-          <input
-            name="imagePath"
-            data-testid="image-input"
-            value={ imagePath }
-            onChange={ this.handleInputOnChange }
-          />
-        </label>
-        <label data-testid="storyline-input-label" htmlFor="storyline-input">
-          Sinopse
-          <textarea name="storyline" data-testid="storyline-input" value={ storyline } onChange={ this.handleInputOnChange } />
-        </label>
-        <label data-testid="rating-input-label" htmlFor="rating-input">
-          Avaliação
-          <input
-            type="number"
-            name="rating"
-            data-testid="rating-input"
-            onChange={ this.handleInputOnChange }
-            value={ rating }
-          />
-        </label>
-        <label data-testid="genre-input-label" htmlFor="genre-input">
-          Gênero
-          <select name="genre" data-testid="genre-input" value={ genre } onChange={ this.handleInputOnChange }>
-            <option data-testid="genre-option" value="action">Ação</option>
-            <option data-testid="genre-option" value="comedy">Comédia</option>
-            <option data-testid="genre-option" value="thriller">Suspense</option>
-          </select>
-        </label>
-        <button type="button" data-testid="send-button" onClick={ this.handleButtonOnClick }>
-          Adicionar filme
-        </button>
+        { this.renderInputTitle }
+        { this.renderInputSubTitle }
+        { this.renderInputImage }
+        { this.renderInputStoryline }
+        { this.renderInputRating }
+        { this.renderInputGenre }
+        { this.renderSendButton }
       </form>
     );
   }
